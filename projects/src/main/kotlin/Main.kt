faf60d2d9d11e404dce1f5f5d2358af119d0575d
DiepvensBent <71712367+DiepvensBent@users.noreply.github.com>
2021-10-05 16:32:43 +0200

    task 3

--- projects/src/main/kotlin/Main.kt
+++ projects/src/main/kotlin/Main.kt
@@ -1,8 +1,11 @@
 fun main () {
     println("Hello World!")
-    var arr = arrayOf("Bent","Gijs","Kylian")
-    arr.forEach { println("Hello ${it}!")}
+    greeting()
 }
-fun greeting (){
-
+fun greeting(){
+    val arr = arrayOf("Bent", "Gijs", "Kylian")
+    arr.forEach { println("Hello $it!") }
+    for (element in arr){
+        println("Hello $element")
+    }
 }
