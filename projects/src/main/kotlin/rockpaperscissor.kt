58a083067966ad7bed8ac06fdae19e7d12449da8
DiepvensBent <71712367+DiepvensBent@users.noreply.github.com>
2021-10-05 17:28:22 +0200

    spock lizard

--- projects/src/main/kotlin/rockpaperscissor.kt
+++ projects/src/main/kotlin/rockpaperscissor.kt
@@ -1,3 +1,34 @@
-fun main () {
-    val arr = arrayOf("Rock","Paper","Scissors")
+fun main(){
+    val options = arrayOf("rock","paper","scissors","lizard","spock")
+    val computerChoice = options.random()
+    val userChoice = getUserChoice(options)
+
+    println("Computer picked $computerChoice")
+    println("User picked $userChoice")
+    determineWinner(computerChoice, userChoice)
+}
+
+fun getUserChoice(options: Array<String>): String? {
+    var userChoice: String?
+    do {
+        println("Make your choice: rock - paper - scissors - lizard - spock")
+        userChoice = readLine()
+    } while(!options.contains(userChoice))
+
+    return userChoice
+}
+
+fun determineWinner(computerChoice: String, userChoice: String?){
+    if (computerChoice == userChoice){
+        println("draw")
+        return
+    }
+    val winMessage ="WINNER WINNER CHICKEN DINNER WOOOOOOOHOOOOOOOOOO"
+    val loseMessage = "loser!"
+
+    when(computerChoice){
+        "rock" -> if (userChoice == "paper" || userChoice == "spock") println(winMessage) else println(loseMessage)
+        "paper" -> if (userChoice == "scissors" || userChoice == "lizard") println(winMessage) else println(loseMessage)
+        "scissors" -> if(userChoice == "rock" || userChoice == "spock") println(winMessage) else println(loseMessage)
+    }
 }
