15a6ea6f4a4ef5a8330095b1a86fa63095895d81
DiepvensBent <71712367+DiepvensBent@users.noreply.github.com>
2021-10-19 15:37:37 +0200

    session3 Client brief finished

---
+++ projects/SESSION3/src/main/kotlin/Course.kt
@@ -0,0 +1,10 @@
+class Course (
+    val Name: String,
+    val Credits: Int,
+    val Teacher: String,
+    val Description: String,
+    val Semesters: Int,
+    val Track: String,
+    val Year: Int
+        ){
+}
---
+++ projects/SESSION3/src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+fun main(){
+    val design1 = Course("Design1",5, "Fenna with last name","a meh course",1,"test",2022)
+    var personArray = arrayOf("Dave","John")
+    println(personArray)
+}
