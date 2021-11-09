736948f0f015b6c1f0064ac7cb96ec210db3e739
DiepvensBent <71712367+DiepvensBent@users.noreply.github.com>
2021-11-09 16:55:07 +0100

    Created database structure session4
    Tables cities, passengers, rides & trains created

---
+++ projects/SESSION4/2122DEV3020.sql
@@ -0,0 +1,65 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+CREATE TABLE `cities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `city_name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `passengers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `passenger_name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `rides` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `train_id` int(11) NOT NULL,
+  `departure_time` datetime NOT NULL,
+  `departure_city` int(11) NOT NULL,
+  `destination_city` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `trains` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `max_speed` int(11) NOT NULL,
+  `max_passengers` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+INSERT INTO `cities` (`id`, `city_name`) VALUES
+(1, 'Brussel');
+INSERT INTO `cities` (`id`, `city_name`) VALUES
+(2, 'Leuven');
+
+
+INSERT INTO `passengers` (`id`, `passenger_name`) VALUES
+(1, 'Bent');
+INSERT INTO `passengers` (`id`, `passenger_name`) VALUES
+(2, 'Gijs');
+INSERT INTO `passengers` (`id`, `passenger_name`) VALUES
+(3, 'Kylian');
+
+
+
+INSERT INTO `trains` (`id`, `type`, `max_speed`, `max_passengers`) VALUES
+(1, 'IC1', 200, 100);
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
