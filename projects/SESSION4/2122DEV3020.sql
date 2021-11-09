fb4a695051a812f529baf798beec21759bcef222
DiepvensBent <71712367+DiepvensBent@users.noreply.github.com>
2021-11-09 17:17:18 +0100

    Update 2122DEV3020.sql
    added passenger rides table

--- projects/SESSION4/2122DEV3020.sql
+++ projects/SESSION4/2122DEV3020.sql
@@ -13,6 +13,13 @@ CREATE TABLE `cities` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
+CREATE TABLE `passenger_rides` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `passenger_id` int(11) NOT NULL,
+  `ride_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
 CREATE TABLE `passengers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `passenger_name` varchar(255) CHARACTER SET utf8 NOT NULL,
@@ -26,7 +33,7 @@ CREATE TABLE `rides` (
   `departure_city` int(11) NOT NULL,
   `destination_city` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `trains` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -42,6 +49,10 @@ INSERT INTO `cities` (`id`, `city_name`) VALUES
 (2, 'Leuven');
 
 
+INSERT INTO `passenger_rides` (`id`, `passenger_id`, `ride_id`) VALUES
+(1, 1, 1);
+
+
 INSERT INTO `passengers` (`id`, `passenger_name`) VALUES
 (1, 'Bent');
 INSERT INTO `passengers` (`id`, `passenger_name`) VALUES
@@ -49,6 +60,8 @@ INSERT INTO `passengers` (`id`, `passenger_name`) VALUES
 INSERT INTO `passengers` (`id`, `passenger_name`) VALUES
 (3, 'Kylian');
 
+INSERT INTO `rides` (`id`, `train_id`, `departure_time`, `departure_city`, `destination_city`) VALUES
+(1, 1, '2021-11-09 17:00:00', 1, 2);
 
 
 INSERT INTO `trains` (`id`, `type`, `max_speed`, `max_passengers`) VALUES
